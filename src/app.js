57140f93bbbb4b3a573dc2edcc74d9fc25fae8e2
Nilton F <jnvlfilho@hotmail.com>
2023-01-03 14:54:24 -0300

    Testing and Implementation the list of users

--- src/app.js
+++ src/app.js
@@ -4,4 +4,13 @@ app.get('/',(req,res) =>{
     res.status(200).send();
 });
 
+app.get('/users',(req,res) =>{
+    const users = [
+        { name: 'Jhon Doe', email: 'jhon@mail.com' },
+    ];
+
+    res.status(200).json(users);
+
+})
+
 module.exports = app;
