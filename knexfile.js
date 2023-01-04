ceca5026632e60e93694ca037842a33ce95a54ab
Nilton F <jnvlfilho@hotmail.com>
2023-01-03 23:51:24 -0300

    Conecting PostgresSQL database and KnexJS

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+module.exports = {
+    test:{
+        client: 'pg',
+        //version: '8.0.3',
+        connection:{
+            host: 'localhost',
+            user: 'postgres',
+            password: 'docker',
+            database: 'nodejstdd'
+        },
+        migrations:{
+            directory: 'src/migrations',
+        }
+    }
+}
