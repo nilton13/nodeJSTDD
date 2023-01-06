597714f6cfb600fffcbf9ced799e3971df3eca1c
Nilton F <jnvlfilho@hotmail.com>
2023-01-05 21:55:12 -0300

    Integration database and Knex ORM in Users Tests

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,15 +1,13 @@
-module.exports = () =>{
+module.exports = (app) =>{
 
     const findAll = (req,res) =>{
-        const users = [
-            { name: 'Jhon Doe', email: 'jhon@mail.com' },
-        ];
-    
-        res.status(200).json(users);
+        app.db('users').select()
+            .then(result => res.status(200).json(result))
     };
     
-    const create = (req,res) =>{
-        res.status(201).json(req.body);
+    const create = async(req,res) =>{
+        const result = await app.db('users').insert(req.body, '*');
+        res.status(201).json(result[0]);
     };
 
     return { findAll,create }
--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -1,19 +1,20 @@
 const request = require('supertest');
 
-const app = require('../src/app');
+const app = require('../../src/app');
 
 test('Deve listar todos o usuários', () =>{
     return request(app).get('/users')
         .then((res) =>{
             expect(res.status).toBe(200);
-            expect(res.body).toHaveLength(1);
-            expect(res.body[0]).toHaveProperty('name','Jhon Doe');
+            expect(res.body.length).toBeGreaterThan(0)
+            //expect(res.body[0]).toHaveProperty('name','Jhon Doe');
         })
 });
 
 test('Deve inserir usuário com sucesso', () =>{
+    const mail = `${Date.now()}@email.com`
     return request(app).post('/users')
-        .send({ name:'Walter Mitty', email: 'walter@email.com' })
+        .send({ name:'Walter Mitty', email: mail,password:'123456' })
         .then((res) =>{
             expect(res.status).toBe(201);
             expect(res.body.name).toBe('Walter Mitty');
