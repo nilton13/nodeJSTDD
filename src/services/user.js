31c7e4a4befdaf7f972ce5fcc3301f18cc1825a0
Nilton F <jnvlfilho@hotmail.com>
2023-01-07 10:17:49 -0300

    Entering User validation

--- src/services/user.js
+++ src/services/user.js
@@ -3,7 +3,15 @@ module.exports = (app) =>{
         return app.db('users').select();
     };
 
-    const save = (user) =>{
+    const save = async (user) =>{
+        if(!user.name) return { error: 'Nome é um atributo obrigatório' }
+        if(!user.email) return { error: 'Email é um atributo obrigatório' }
+        if(!user.password) return { error: 'Senha é um atributo obrigatório' }
+
+        const userDb = await app.db('users').where({email: user.email}).select();
+        
+        if(userDb && userDb.length > 0) return { error: 'Já existe um usuário com esse email' }
+
         return app.db('users').insert(user,'*');
     }
 
--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -2,6 +2,8 @@ const request = require('supertest');
 
 const app = require('../../src/app');
 
+const mail = `${Date.now()}@email.com`;
+
 test('Deve listar todos o usuários', () =>{
     return request(app).get('/users')
         .then((res) =>{
@@ -12,11 +14,48 @@ test('Deve listar todos o usuários', () =>{
 });
 
 test('Deve inserir usuário com sucesso', () =>{
-    const mail = `${Date.now()}@email.com`
     return request(app).post('/users')
         .send({ name:'Walter Mitty', email: mail,password:'123456' })
         .then((res) =>{
             expect(res.status).toBe(201);
             expect(res.body.name).toBe('Walter Mitty');
         })  
-})
+});
+
+//Testes de validação de usuário
+test('Não deve inserir usuário sem nome', () => {
+    return request(app).post('/users')
+        .send({ email: 'nilton@teste.com', password: '123456' })
+        .then((res) =>{
+            expect(res.status).toBe(400);
+            expect(res.body.error).toBe('Nome é um atributo obrigatório');
+        })
+});
+
+test('Não deve inserir usuário sem email', async () =>{
+    const result = await request(app).post('/users')
+        .send({ name:'Walter Mitty', password:'123456' });
+    
+        expect(result.status).toBe(400);
+        expect(result.body.error).toBe('Email é um atributo obrigatório')
+});
+
+test('Não deve inserir usuário sem senha', (done) => {
+    request(app).post('/users')
+        .send({ name:'Walter Mitty', email: 'nilton@teste.com'})
+        .then((res) =>{
+            expect(res.status).toBe(400);
+            expect(res.body.error).toBe('Senha é um atributo obrigatório');
+            done();
+        })
+
+});
+
+test('Não deve inserir usuário com Email existente',() =>{
+    return request(app).post('/users')
+        .send({ name:'Walter Mitty', email: mail,password:'123456' })
+        .then((res) =>{
+            expect(res.status).toBe(400);
+            expect(res.body.error).toBe('Já existe um usuário com esse email');
+        })  
+});
